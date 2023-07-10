60deaacd76a5ea17c60d32c0fa3c699f36d749d6
Veikka <Veikka.puolitaival@gmail.com>
2023-07-11 00:54:57 +0300

    tailwindcss setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,2 +1,42 @@
-import react from "@vitejs/plugin-react-swc";
+import { useState } from 'react';
 
+function Home() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim() !== '') {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const handleDelete = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={task} onChange={handleChange} />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
