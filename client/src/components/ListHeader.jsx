bb82cccd42b6b5a6182d30fddf8fa66c0e8df43f
Veikka <Veikka.puolitaival@gmail.com>
2023-07-27 00:54:17 +0300

    SignOut

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "../App.css";
 import Modal from "./Modal";
+import { useCookies } from "react-cookie";
 
 const ListHeader = ({ listName, getData }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
   const [showModal, setShowModal] = useState(false);
 
   const signOut = () => {
     console.log("sign out");
+    removeCookie("Email");
+    removeCookie("AuthToken");
+    window.location.reload();
   };
 
   return (
